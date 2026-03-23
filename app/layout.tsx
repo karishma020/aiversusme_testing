e317e8152f097645ce1c14f98f2c8b10df30583f
karishma020 <karishmakotecha10@gmail.com>
2026-03-23 16:08:31 +0530

    fixed a typo error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
-import "./gAI Take My Job?lobals.css";
+import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import Header from "@/components/header";
